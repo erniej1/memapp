d4e3e9d1258af45903dbd997a62e7df76b7c0e05
Ernie <ernestjohnson80@yahoo.com>
2020-12-13 19:44:59 -0800

    Update posts.js

--- controller/posts.js
+++ controller/posts.js
@@ -28,7 +28,7 @@ export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No posty with dat iddee');
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with dat id');
 
   const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
@@ -42,7 +42,7 @@ export const deletePost = async (req, res) => {
 
   await PostMessage.findByIdAndRemove(id);
 
-  res.json({ message: 'Post deleted goodly' });
+  res.json({ message: 'Post deleted' });
 };
 
 export const likePost = async (req, res) => {
